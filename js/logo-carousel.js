cfb1be7da4accf2ae46c4adfcc712c0f44fc002e
aml <amlamer2001@gmail.com>
2024-03-25 21:13:24 +0200

    upload

--- js/logo-carousel.js
+++ js/logo-carousel.js
@@ -1,32 +1,9 @@
 /// <reference types="./../@types/jquery" />
-
-// $(document).ready(function() {
-//     $('.logo-carousel').slick({
-//       slidesToShow: 6,
-//       slidesToScroll: 1,
-//       autoplay: true,
-//       autoplaySpeed: 1000,
-//       arrows: true,
-//       dots: false,
-//       pauseOnHover: false,
-//       responsive: [{
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 4
-//         }
-//       }, {
-//         breakpoint: 520,
-//         settings: {
-//           slidesToShow: 2
-//         }
-//       }]
-//     });
-//   });
-
     $('.owl-carousel').owlCarousel({
         items:5,
         loop:true,
         autoplay:true,
+        autoplayTimeout:2000,
         responsive:{
             0:{
                 items:3,
