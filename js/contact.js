ce158ee40a4bdf880dafc52de41658cc7f43dc9d
aml <amlamer2001@gmail.com>
2024-05-03 19:02:50 +0300

    commit

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+let submit=document.getElementById('btn-submit')
+submit.addEventListener('submit',function(){
+    // let name=document.getElementById("name").value;
+
+})
+
+
+function nameValidation(){
+    let name=document.getElementById("name").value;
+}
+
+function passwordValidation(){
+    
+}
+
+function textValidation(){
+    
+}
